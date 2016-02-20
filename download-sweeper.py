9617f7c32c0cd40b2e986a1688859e5942670c50
Brandon Milton <brandon.milton21@gmail.com>
2016-02-20 14:46:22 -0800

    Added download sweeper executable

---
+++ download-sweeper.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+################################################################################
+# Filename: download-sweeper.py
+# Author:   Brandon Milton
+#           http://brandonio21.com
+# Date:     20 February 2016
+# 
+# This file processes files in the downloads folders specified in the 
+# configuration and moves them or removes them according to the user's spec.
+################################################################################
+import argparse
+import yaml
+
+
+
+
+if __name__ == "__main__":
