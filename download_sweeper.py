8bd52c53bb209fa8cc9f39d7b8c354bfccbe5b7a
Brandon Milton <brandon.milton21@gmail.com>
2017-04-03 12:43:39 -0700

    Remove 3 from python pre-amble

--- download_sweeper.py
+++ download_sweeper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 ###############################################################################
 # Filename: download-sweeper.py
 # Author:   Brandon Milton
